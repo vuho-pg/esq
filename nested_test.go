054ae1f9eca8f0389fda5e972359a319710f2376
vuhk <qwerty22121998@gmail.com>
2023-06-15 03:10:42 +0700

    enum

--- nested_test.go
+++ nested_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestNested(t *testing.T) {
-	n := Nested("register_services", Bool().Filter(MatchPhrase("register_services.parent_merchant_code", "NCCDF2GR"))).ScoreMode("avg")
+	n := Nested("register_services", Bool().Filter(MatchPhrase("register_services.parent_merchant_code", "NCCDF2GR"))).ScoreMode(ScoreModeAvg)
 	j, _ := json.MarshalIndent(n, "", "\t")
 	t.Log(string(j))
 }
